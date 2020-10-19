14c5ca54876142276bfd31115a290f43c23cc0e9
iMorreeu <70594197+iMorreeu@users.noreply.github.com>
2020-10-19 11:37:59 -0300

    NLW ROCKETSEAT

---
+++ public/scripts/page-create-orphanage.js
@@ -0,0 +1,70 @@
+//create map
+const map = L.map("mapid").setView([-18.7259831, -47.5065206], 15);
+// create and add tileLayer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+// create icon
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  
+});
+
+let marker;
+
+
+//create and add marker
+map.on('click', (event) => {
+    const lat = event.latlng.lat
+    const lng = event.latlng.lng
+
+    document.querySelector('[name=lat]').value = lat
+    document.querySelector('[name=lng]').value = lng
+
+    //remove icon
+    marker && map.removeLayer(marker)
+
+    //add icon layer
+    marker = L.marker([lat, lng], {icon})
+    .addTo(map)
+}
+)
+
+//add photo camp
+function addPhotoField(){
+    //pegar container de fotos #images
+    const container = document.querySelector('#images')
+    //pegar o container para duplicar .new-upload
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+
+    //realizar o clone da ultima imagem adicionada
+    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true)
+    
+    //verificar se o campo esta vazio, se sim não adicionar ao container de imagens
+    const input = newFieldContainer.children[0]
+    if(input.value == ''){
+        alert('Coloque um link antes de adicionar')
+        return 
+    }
+
+    //limpar o campo antes de adicionar ao container de imagens
+    input.value=''
+
+    //adicionar o clone ao container de imagens
+    container.appendChild(newFieldContainer)
+}
+
+function deletField(event){
+    const span = event.currentTarget
+
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+    // limpar o campo 
+    span.parentNode.children[0].value =''
+    
+    console.log(fieldsContainer)
+    if(fieldsContainer.length <= 1){
+        return
+    }
+    span.parentNode.remove()
+
+}
